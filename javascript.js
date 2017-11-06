972325fb2c715a52bda85b4ed0667657c8504139
Kestas <kaskas@users.noreply.github.com>
2017-11-06 11:57:55 -0800

    console output

--- javascript.js
+++ javascript.js
@@ -3,3 +3,8 @@
 if (x === 0) {
     // doSomething()...
 }
+
+//console output
+console.log("Log a debug level message.");
+console.warn("Log a warn level message.");
+console.error("Log an error level message.");
